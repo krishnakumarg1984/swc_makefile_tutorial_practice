92249fc2f1bfb2b1b2b02a181ec2a63aade8ef43
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-06-15 19:09:00 +0100

    black formatting applied to testzip.py

--- 01_introduction/testzipf.py
+++ 01_introduction/testzipf.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-from countwords import load_word_counts
 import sys
+
+from countwords import load_word_counts
+
 
 def top_two_word(counts):
     """
-    Given a list of (word, count, percentage) tuples, 
+    Given a list of (word, count, percentage) tuples,
     return the top two word counts.
     """
     limited_counts = counts[0:2]
@@ -12,12 +14,11 @@ def top_two_word(counts):
     return count_data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     input_files = sys.argv[1:]
     print("Book\tFirst\tSecond\tRatio")
     for input_file in input_files:
         counts = load_word_counts(input_file)
         [first, second] = top_two_word(counts)
         bookname = input_file[:-4]
-        print("%s\t%i\t%i\t%.2f" %(bookname, first, second, float(first)/second))
-
+        print("%s\t%i\t%i\t%.2f" % (bookname, first, second, float(first) / second))
