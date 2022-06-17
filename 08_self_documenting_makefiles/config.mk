45ffbc6b73c671d16049e65e4710f14ab0081e3a
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-06-17 20:56:11 +0100

    sets up variables PLOT_SRC, PLOT_EXE and rules to make and clean png files

--- 08_self_documenting_makefiles/config.mk
+++ 08_self_documenting_makefiles/config.mk
@@ -6,3 +6,7 @@ COUNT_EXE=$(LANGUAGE) $(COUNT_SRC)
 # Test Zipf's rule
 ZIPF_SRC=testzipf.py
 ZIPF_EXE=$(LANGUAGE) $(ZIPF_SRC)
+
+# Plot word count
+PLOT_SRC=plotcounts.py
+PLOT_EXE=$(LANGUAGE) $(PLOT_SRC)
