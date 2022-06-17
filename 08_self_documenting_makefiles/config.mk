57fb3748a4c35c6d57fa2e9be5a0d4b282a79b5f
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-06-17 22:47:39 +0100

    adds final makefile and reference solution for archive creation

--- 08_self_documenting_makefiles/config.mk
+++ 08_self_documenting_makefiles/config.mk
@@ -10,3 +10,13 @@ ZIPF_EXE=$(LANGUAGE) $(ZIPF_SRC)
 # Plot word count
 PLOT_SRC=plotcounts.py
 PLOT_EXE=$(LANGUAGE) $(PLOT_SRC)
+
+ARCHIVE_DIR=zipf_analysis
+ARCHIVE_FILE=$(ARCHIVE_DIR).tar.gz
+RESULTS_FILE=results.txt
+TXT_DIR=books
+
+TXT_FILES=$(wildcard $(TXT_DIR)/*.txt)
+DAT_FILES=$(patsubst books/%.txt, %.dat, $(TXT_FILES))
+PNG_FILES=$(patsubst %.dat, %.png, $(DAT_FILES))
+
